ac9d86d634bbfd6032c9046ab9d2f69e813804ba
Eneapoison <ennexxgame@gmail.com>
2023-01-18 12:55:20 +0100

    add

---
+++ phpenea/variabili/variabili.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variabili</title>
+</head>
+
+<body>
+    <?php
+   // $var1 = "1";
+    //echo $var1;
+    //$var1 = "stringa";
+    //echo $var1;
+    //$var2 = "pippo";
+    //echo $var2;
+    //echo "$var1 <br> $var2" //sostituisce il nome
+    //echo "Ciao a tutti" ;
+   // echo ' <br> \' la variabile $var1 = ' . "$var1";
+   // echo " <br> simbolo speciale"
+
+
+
+    //PER CASA
+    //ASSEGNARE AD UNA VARIABILE LA STRINGA "andare a casa"
+    //SCRIVERE IN UNA SOLA ECHO
+    //l'insegnante dice "è ora di" (CONTENUTO DELLA VARIABILE)
+    
+    $var1 = "andare a casa";
+    echo "è ora di &nbsp"; print $var1;
+
+    ?>
+</body>
+
+</html>
