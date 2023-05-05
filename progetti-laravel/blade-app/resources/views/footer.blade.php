c80e112dceb831fbffe2db6d40f065128400091c
Eneapoison <ennexxgame@gmail.com>
2023-05-05 14:07:32 +0200

    add

---
+++ progetti-laravel/blade-app/resources/views/footer.blade.php
@@ -0,0 +1,123 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i>Enea Amadei
+                    </h6>
+                    <p>
+                        Benvenuti nel gestionale Hotel di Enea!!!
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Prodotti
+                    </h6>
+                    <p>
+                        <a href="https://www.teamexos.com/" class="text-reset" target="_blank">Palestra</a>
+                    </p>
+                    <p>
+                        <a href="https://www.aquafan.it/" class="text-reset" target="_blank">Piscina</a>
+                    </p>
+                    <p>
+                        <a href="https://www.gamestop.it/" class="text-reset" target="_blank">Sala Giochi</a>
+                    </p>
+                    <p>
+                        <a href="https://www.civitatis.com/f/estados-unidos/nueva-york/guia/central-park.jpg"
+                            class="text-reset" target="_blank">Parco</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Link
+                    </h6>
+                    <p>
+                        <a href="{{ asset('404ita.png') }}" target="_blank" class="text-reset">Prenota</a>
+                    </p>
+                    <p>
+                        <a href="{{ asset('404ita.png') }}" target="_blank" class="text-reset">Impostazioni</a>
+                    </p>
+                    <p>
+                        <a href="{{ asset('404ita.png') }}" target="_blank" class="text-reset">Visita</a>
+                    </p>
+                    <p>
+                        <a href="{{ asset('404ita.png') }}" target="_blank" class="text-reset">Aiuto</a>
+
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">Contatti</h6>
+                    <p><i class="fas fa-home me-3"></i> Rimini, RN 47922, ITA</p>
+                    <p>
+                        <i class="fas fa-envelope me-3"></i>
+                        eneahotels@gmail.com
+                    </p>
+                    <p><i class="fas fa-phone me-3"></i> + 0541 487 669 487</p>
+                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2023 Copyright:
+        <a class="text-reset fw-bold" href="https://www.google.it/" target="_blank">Google.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
